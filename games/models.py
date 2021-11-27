8bcf2728f95db19357bef6cb7b6ba7ab334e4949
Tomek Karwowski <to.karwowski@gmail.com>
2021-11-27 18:55:39 +0100

    add fields to models

--- games/models.py
+++ games/models.py
@@ -5,6 +5,12 @@ from django.db import models
 
 class Game(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    files = models.FileField()
+    accepted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Game {self.id}"
+        return f"Game {self.name}"
--- rooms/models.py
+++ rooms/models.py
@@ -2,9 +2,14 @@ import uuid
 
 from django.db import models
 
+from games.models import Game
+
 
 class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    game = models.ForeignKey(Game, on_delete=models.PROTECT)
+    max_players = models.PositiveSmallIntegerField(default=10)
+    current_players = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return f"Room {self.id}"
